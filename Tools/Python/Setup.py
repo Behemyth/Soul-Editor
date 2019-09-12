e08268a4a633cabe664a1a2fa774f5fc601b577a
Asher Norland <asher.norland@gmail.com>
2019-09-12 10:24:36 -0400

    Set Colour

--- Tools/Python/Setup.py
+++ Tools/Python/Setup.py
@@ -7,8 +7,6 @@ from Utilities import Task
 
 def Setup():
 
-    print("Updating the C++ environment")
-
     buildPath = Path().absolute() / "Build"
     buildPathString = str(buildPath)
 
@@ -16,14 +14,12 @@ def Setup():
     conanFilePathString = str(conanFilePath)
 
     #Set the conan remote
-    Task("conan", "remote", "add", "--force", "bincrafters", "https://api.bintray.com/conan/bincrafters/public-conan")
+    Task("Conan: Set Remotes", "conan", "remote", "add", "--force", "bincrafters", "https://api.bintray.com/conan/bincrafters/public-conan")
 
     #Create build directory if it does not exist
     if not os.path.exists(buildPath):
         os.makedirs(buildPath)
 
     #install conan dependencies
-    Task("conan", "install", conanFilePathString, "-if", buildPathString,"-l", conanFilePathString, "-s", "build_type=Debug")
-    Task("conan", "install", conanFilePathString, "-if", buildPathString,"-l", conanFilePathString, "-s", "build_type=Release")
-
-    print("Finished setup")
+    Task("Conan: Install Debug Dependencies", "conan", "install", conanFilePathString, "-if", buildPathString,"-l", conanFilePathString, "-s", "build_type=Debug")
+    Task("Conan: Install Release Dependencies", "conan", "install", conanFilePathString, "-if", buildPathString,"-l", conanFilePathString, "-s", "build_type=Release")
